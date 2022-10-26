65d9fd3a039e39779bca34fe50b16486226c4d30
dev-venkateshnagumantri <venkateshnagumantri01@gmail.com>
2022-10-26 20:20:44 +0530

    modified allowed hosts

--- cars_world/settings.py
+++ cars_world/settings.py
@@ -29,7 +29,7 @@ DEBUG = True
 
 
 
-ALLOWED_HOSTS = ['thecarzone.herokuapp.com','thecarzone.co','www.thecarzone.co','localhost']
+ALLOWED_HOSTS = ['thecarzone.herokuapp.com','thecarzone.co','www.thecarzone.co']
 
 LOGIN_URL = 'pages:login'
 LOGOUT_URL = 'pages:logout'
