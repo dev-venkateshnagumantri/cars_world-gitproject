6be3daf71312c088e93fc7b5d9bbc7fcdb7cdfe8
dev-venkateshnagumantri <venkateshnagumantri01@gmail.com>
2022-10-26 11:11:38 +0530

    changed settings again

--- cars_world/settings.py
+++ cars_world/settings.py
@@ -27,6 +27,8 @@ SECRET_KEY = 'django-insecure-y!0*4-hv1ie@uuqf&q9qybr%f(%p!fm_lt%5a=dzo)05#-z!8g
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
+DEBUG_PROPAGATE_EXCEPTIONS = True
+
 ALLOWED_HOSTS = ["thecarzone.herokuapp.com","thecarzoneapp.co","www.thecarzoneapp.co",'127.0.0.1','localhost']
 
 LOGIN_URL = 'pages:login'
@@ -162,6 +164,7 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
     
 }
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
