2fdd8501d99c4b8530bc422e73fd6b0c0d39cbf0
dev-venkateshnagumantri <venkateshnagumantri01@gmail.com>
2022-10-28 19:03:48 +0530

    changed debug to false

--- cars_world/settings.py
+++ cars_world/settings.py
@@ -25,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-y!0*4-hv1ie@uuqf&q9qybr%f(%p!fm_lt%5a=dzo)05#-z!8g'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 
 
