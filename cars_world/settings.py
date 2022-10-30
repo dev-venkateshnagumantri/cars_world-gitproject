7cbd37aeac7027ed4fdb8caa96f583ed5f619f31
dev-venkateshnagumantri <venkateshnagumantri01@gmail.com>
2022-10-30 12:50:21 +0530

    final commit 3

--- cars_world/settings.py
+++ cars_world/settings.py
@@ -31,7 +31,6 @@ SECRET_KEY = 'django-insecure-y!0*4-hv1ie@uuqf&q9qybr%f(%p!fm_lt%5a=dzo)05#-z!8g
 DEBUG = False
 
 
-
 ALLOWED_HOSTS = ['thecarzone.herokuapp.com','thecarzone.co','www.thecarzone.co','localhost']
 
 LOGIN_URL = 'pages:login'
