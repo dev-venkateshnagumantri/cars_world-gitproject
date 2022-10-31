95d469e96d4d33a438dbfe133b304b410d780380
dev-venkateshnagumantri <venkateshnagumantri01@gmail.com>
2022-10-31 19:18:53 +0530

    404 customized final

--- cars_world/views.py
+++ cars_world/views.py
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 import django
 
 def custom_page_not_found(request,exception):
-=======
-import django
-
-def custom_page_not_found(request,exception):
->>>>>>> c1fe6d6fd59363b222f52a4e4f621e67e554befd
      return django.views.defaults.page_not_found(request, None)
